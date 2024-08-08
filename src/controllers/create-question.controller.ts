7ee2c7d3153b051e33fb3b351020397a63dc6265
Helder Costa <heldercostaaa@gmail.com>
2024-08-07 22:55:34 -0300

    feat: implement create question controller logic

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,17 +1,15 @@
+import { CurrentUser } from '@/auth/current-user.decorator';
 import { JwtGuard } from '@/auth/jwt.guard';
+import { UserPayload } from '@/auth/jwt.strategy';
 import { ZodValidationPipe } from '@/pipes/zod-validation.pipe';
 import { PrismaService } from '@/prisma/prisma.service';
-import {
-  Body,
-  Controller,
-  HttpCode,
-  Post,
-  UseGuards,
-  UsePipes,
-} from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
 import z from 'zod';
 
-const bodySchema = z.object({});
+const bodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
 
 type Body = z.infer<typeof bodySchema>;
 
@@ -22,6 +20,34 @@ export class CreateQuestionController {
 
   @Post()
   @HttpCode(201)
-  @UsePipes(new ZodValidationPipe(bodySchema))
-  async handle(@Body() body: Body) {}
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Body(new ZodValidationPipe(bodySchema)) body: Body,
+  ) {
+    const userId = user.sub;
+    const { title, content } = body;
+
+    await this.prisma.question.create({
+      data: {
+        authorId: userId,
+        title,
+        content,
+        slug: this.convertToSlug(title),
+      },
+    });
+  }
+
+  private convertToSlug(text: string) {
+    const slug = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '');
+
+    return slug;
+  }
 }
