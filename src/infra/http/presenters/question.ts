47fce52c9cc2272c10f5e1cf00979a1b8cac9b10
Helder Costa <heldercostaaa@gmail.com>
2024-08-09 21:29:02 -0300

    feat: implement register and authenticate student use cases

--- src/infra/http/presenters/question.ts
+++ src/infra/http/presenters/question.ts
@@ -4,6 +4,7 @@ export class QuestionPresenter {
   static toHTTP(question: Question) {
     return {
       id: question.id.toString(),
+      authorId: question.authorId.toString(),
       title: question.title,
       content: question.content,
       slug: question.slug.value,
