e27e882517c3447ce82179a3f065f0780aab2eef
Helder Costa <heldercostaaa@gmail.com>
2024-08-21 19:16:52 -0300

    fix: fix tsc errors

--- src/domain/notification/application/subscribers/on-answer-created.spec.ts
+++ src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -1,12 +1,15 @@
+import { MockInstance } from 'vitest';
+
 import { makeAnswer } from 'test/factories/make-answer';
 import { makeQuestion } from 'test/factories/make-question';
 import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments';
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers';
+import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-attachments';
 import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications';
 import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments';
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions';
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository';
 import { waitFor } from 'test/utils/wait-for';
-import { MockInstance } from 'vitest';
 import { SendNotificationUseCase } from '../use-cases/send-notification';
 import { OnAnswerCreated } from './on-answer-created';
 
@@ -15,6 +18,8 @@ let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository;
 let inMemoryAnswersRepository: InMemoryAnswersRepository;
 let inMemoryNotificationsRepository: InMemoryNotificationsRepository;
+let inMemoryStudentsRepository: InMemoryStudentsRepository;
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository;
 let sendNotificationUseCase: SendNotificationUseCase;
 
 let sendNotificationExecuteSpy: MockInstance<
@@ -25,8 +30,12 @@ describe('On Answer Created', () => {
   beforeEach(() => {
     inMemoryQuestionAttachmentsRepository =
       new InMemoryQuestionAttachmentsRepository();
+    inMemoryStudentsRepository = new InMemoryStudentsRepository();
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository();
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
       inMemoryQuestionAttachmentsRepository,
+      inMemoryAttachmentsRepository,
+      inMemoryStudentsRepository,
     );
     inMemoryAnswerAttachmentsRepository =
       new InMemoryAnswerAttachmentsRepository();
